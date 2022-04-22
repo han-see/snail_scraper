8c7aa1670a674fb4aa1b39164ed6b0f239575bc2
han-see <74469036+han-see@users.noreply.github.com>
2022-04-22 21:12:37 +0200

    Add interface and enum for the data

---
+++ src/common/Family.ts
@@ -0,0 +1,62 @@
+export enum Family {
+  Garden = 1,
+  Helix = 2,
+  Milk = 3,
+  Agate = 4,
+  Atlantis = 5,
+}
+export enum Klass {
+  Novice = 1,
+  Initiate = 2,
+  Apprentice = 3,
+  Expert = 4,
+  Master = 5,
+  Special = 6,
+  Genesis = 7,
+}
+export enum Adaptations {
+  Desert = 1,
+  Mountain = 2,
+  Beach = 3,
+  Glacier = 4,
+  Forest = 5,
+  Space = 6,
+  Hot = 40,
+  Cold = 41,
+  Wind = 42,
+  Wet = 43,
+  Snow = 44,
+  Storm = 45,
+  Slide = 80,
+  Jump = 81,
+  Roll = 82,
+  Dodge = 83,
+}
+export enum Gender {
+  Female = 1,
+  Male = 2,
+}
+// So dirty like snail
+export type Generation = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+// This one even dirtier
+export type Purity =
+  | 1
+  | 2
+  | 3
+  | 4
+  | 5
+  | 6
+  | 7
+  | 8
+  | 9
+  | 10
+  | 11
+  | 12
+  | 13
+  | 14
+  | 15
+  | 16
+  | 17
+  | 18
+  | 19
+  | 20;
