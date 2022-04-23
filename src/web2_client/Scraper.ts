14b72a0afd746d671b8677df74a2853dab37817b
han-see <74469036+han-see@users.noreply.github.com>
2022-04-23 13:44:40 +0200

    Create Scraper class

--- src/web2_client/Scraper.ts
+++ src/web2_client/Scraper.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 import { Marketplace, Snail } from '../common/MarketplaceResponse';
 import * as fs from 'fs';
 import 'dotenv/config';
-import { Adaptations, Family } from '../common/Family';
-import { QueryAllSnail, QueryFilter } from './Query';
+import { IQueryFilter, QueryAllSnail, QueryFilter } from './Query';
 import { Webhook } from '../common/Webhook';
+import { UserInput } from '../common/UserInput';
 
 const URL = 'https://api.snailtrail.art/graphql/';
 
@@ -32,51 +32,73 @@ const config = {
   },
 };
 
-const queryFilter: QueryFilter = {};
+export class Scraper {
+  private queryFilter: IQueryFilter;
 
-const queryAllSnail = new QueryAllSnail(queryFilter, 20);
+  private queryAllSnail;
 
-const pingedSnail: number[] = [];
+  private pingedSnail: number[] = [];
 
-export async function scrapeMarketplace() {
-  try {
-    const res = await axios.post<Marketplace>(URL, queryAllSnail, config);
-    const data = JSON.stringify(res.data);
-    await fs.writeFileSync('response.json', data);
-    console.log(`Status: ${res.status}`);
-    checkMarketPlacePrice(res.data);
-  } catch (err) {
-    console.error(err);
+  private discount: number;
+
+  private maxPrice: number;
+
+  constructor(userInput: UserInput) {
+    this.queryFilter = userInput.filter;
+    this.discount = userInput.discount;
+    this.maxPrice = userInput.maxPrice;
+    this.queryAllSnail = new QueryAllSnail(this.queryFilter, 20);
   }
-}
 
-function checkMarketPlacePrice(data: Marketplace) {
-  const snailData: Snail[] = data.data.marketplace_promise.snails;
-  const discount = parseInt(process.env.SNAIL_DISCOUNT);
-  const maxAvaxPrice = parseInt(process.env.SNAIL_MAX_PRICE);
-
-  if (snailData.length > 1) {
-    if (
-      snailData[0].market.price <= snailData[1].market.price * (1 - discount) &&
-      snailData[0].market.on_sale
-    ) {
-      pingUserForDiscount(snailData[0]);
+  async scrapeMarketplace() {
+    try {
+      const res = await axios.post<Marketplace>(
+        URL,
+        this.queryAllSnail,
+        config,
+      );
+      const data = JSON.stringify(res.data);
+      await fs.writeFileSync('response.json', data);
+      console.log(`Status: ${res.status}`);
+      console.log(`Discount: ${this.discount}`);
+      console.log(`Price: ${this.maxPrice}`);
+      console.log(`Filter: ${JSON.stringify(this.queryFilter)}`);
+      console.log('-----------------');
+      this.checkMarketPlacePrice(res.data);
+    } catch (err) {
+      console.error(err);
     }
   }
-  if (snailData[0].market.price <= maxAvaxPrice) {
-    pingUserForDiscount(snailData[0]);
-  }
-}
 
-function pingUserForDiscount(snail: Snail) {
-  if (!pingedSnail.includes(snail.id)) {
-    const webhook = new Webhook(
-      `${snail.name} is on sale for ${snail.market.price} AVAX`,
-      `Adaptations: ${snail.adaptations}`,
-      `https://www.snailtrail.art/snails/${snail.id}/snail`,
-      snail.image,
-    );
-    webhook.sendMessage();
-    pingedSnail.push(snail.id);
+  checkMarketPlacePrice(data: Marketplace) {
+    const snailData: Snail[] = data.data.marketplace_promise.snails;
+    const discount = this.discount;
+    const maxAvaxPrice = this.maxPrice;
+
+    if (snailData.length > 1) {
+      if (
+        snailData[0].market.price <=
+          snailData[1].market.price * (1 - discount) &&
+        snailData[0].market.on_sale
+      ) {
+        this.pingUserForDiscount(snailData[0]);
+      }
+    }
+    if (snailData[0].market.price <= maxAvaxPrice) {
+      this.pingUserForDiscount(snailData[0]);
+    }
+  }
+
+  pingUserForDiscount(snail: Snail) {
+    if (!this.pingedSnail.includes(snail.id)) {
+      const webhook = new Webhook(
+        `${snail.name} is on sale for ${snail.market.price} AVAX`,
+        `Adaptations: ${snail.adaptations}`,
+        `https://www.snailtrail.art/snails/${snail.id}/snail`,
+        snail.image,
+      );
+      webhook.sendMessage();
+      this.pingedSnail.push(snail.id);
+    }
   }
 }
