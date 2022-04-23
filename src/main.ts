697c15de13573731513733d4e4c69033599410f6
han-see <74469036+han-see@users.noreply.github.com>
2022-04-23 13:45:01 +0200

    Add initiate bot function

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,35 @@
-import { scrapeMarketplace } from './web2_client/Scraper';
+import { Scraper } from './web2_client/Scraper';
+import userInput from '../userInput.json';
+
+const scraperList: Scraper[] = [];
 
 async function botLoop() {
   // eslint-disable-next-line no-constant-condition
-  while (true) {
+  await initiateBot();
+  const runBot = true;
+  while (runBot) {
     console.log('Running the loop');
     await new Promise((resolve) => setTimeout(resolve, 1000));
-    scrapeMarketplace();
+    scraperList.forEach((res) => res.scrapeMarketplace());
+  }
+}
+
+async function initiateBot() {
+  try {
+    let isUserInputEmpty = true;
+    while (isUserInputEmpty) {
+      if (userInput.length == 0) {
+        console.log('No User Input');
+        process.exit(1);
+      } else {
+        isUserInputEmpty = false;
+      }
+    }
+    await userInput.forEach((data) => {
+      scraperList.push(new Scraper(data));
+    });
+  } catch (err) {
+    console.error('Bot cannot be initialize', err);
   }
 }
 
