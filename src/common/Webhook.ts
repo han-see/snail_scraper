96b8ddd111064c6614b89f81e9b03055ac8c51f4
han-see <74469036+han-see@users.noreply.github.com>
2022-04-24 15:07:36 +0200

    Add new data to the discord message

--- src/common/Webhook.ts
+++ src/common/Webhook.ts
@@ -1,8 +1,9 @@
-import { MessageEmbed, WebhookClient } from 'discord.js';
+import { MessageEmbed, MessageEmbedFooter, WebhookClient } from 'discord.js';
 import 'dotenv/config';
 
 export class Webhook {
   embed: MessageEmbed;
+  footer: MessageEmbedFooter;
   webhookClient = new WebhookClient({ url: process.env.DISCORD_WEBBOOK });
 
   constructor(
@@ -10,13 +11,18 @@ export class Webhook {
     private content: string,
     private snailLink: string,
     private thumbnail: string,
+    private nextSnailPrice?: number,
   ) {
     this.embed = new MessageEmbed()
       .setTitle(this.title)
       .setColor('#3D9140')
       .setDescription(this.content)
       .setURL(this.snailLink)
-      .setThumbnail(this.thumbnail);
+      .setThumbnail(this.thumbnail)
+      .setTimestamp();
+    this.embed.footer = {
+      text: `Next Snail in Category: ${this.nextSnailPrice} AVAX`,
+    };
   }
 
   sendMessage() {
