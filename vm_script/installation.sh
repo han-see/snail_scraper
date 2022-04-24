3851094ae376de73ceee8ee720c3c6e4d7bf36be
han-see <74469036+han-see@users.noreply.github.com>
2022-04-24 17:29:18 +0200

    Add script and readme

--- vm_script/installation.sh
+++ vm_script/installation.sh
@@ -1,7 +1,9 @@
  #!/bin/bash
 set -x
 git clone git@github.com:han-see/snail_scraper.git
-cd snail_scraper/
+cd snail_scraper/vm_script
+chmod +x *.sh
+cd ..
 npm install
 nano .env.example
 nano userInput.example
