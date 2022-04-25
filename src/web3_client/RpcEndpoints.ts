b4010a23e90c1f76dd80ce590ee2238babe82b63
han-see <74469036+han-see@users.noreply.github.com>
2022-04-25 23:43:03 +0200

    Add new rpc endpoint class

---
+++ src/web3_client/RpcEndpoints.ts
@@ -0,0 +1,9 @@
+// Moralis Endpoint Hansee
+export const MORALIS =
+  'wss://speedy-nodes-nyc.moralis.io/5a4d76c5b7eb6c76fe360efc/avalanche/mainnet/ws';
+
+// Default AVAX RPC endpoint
+export const AVAX_NODE = 'https://api.avax.network/ext/bc/C/rpc';
+
+// Default AVAX RPC endpoint, for websockets
+export const AVAX_WS_NODE = 'wss://api.avax.network/ext/bc/C/ws';
