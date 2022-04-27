62109d4e1d1916c594f1296cdb9baf0e32e94b5b
han-see <74469036+han-see@users.noreply.github.com>
2022-04-27 23:32:42 +0200

    Refactor import

---
+++ src/web2_client/Webhook.ts
@@ -0,0 +1,37 @@
+import { MessageEmbed, MessageEmbedFooter, WebhookClient } from 'discord.js';
+import 'dotenv/config';
+export class Webhook {
+  embed: MessageEmbed;
+  footer: MessageEmbedFooter;
+  webhookClient = new WebhookClient({ url: process.env.DISCORD_WEBBOOK });
+
+  constructor(
+    private title: string,
+    private content: string,
+    private snailLink: string,
+    private thumbnail: string,
+    private nextSnailPrice?: number,
+  ) {
+    this.embed = new MessageEmbed()
+      .setTitle(this.title)
+      .setColor('#3D9140')
+      .setDescription(this.content)
+      .setURL(this.snailLink)
+      .setThumbnail(this.thumbnail)
+      .setTimestamp();
+    this.embed.footer = {
+      text: `Next Snail's price in Category: ${this.nextSnailPrice} AVAX`,
+    };
+  }
+
+  sendMessage() {
+    this.webhookClient
+      .send({
+        content: `<@${process.env.DISCORD_USER_ID}>\n${this.embed.title}`,
+        embeds: [this.embed],
+      })
+      .catch((err) => {
+        console.error('Cannot send message to discord', err);
+      });
+  }
+}
