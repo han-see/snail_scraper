aec632f220441dc76d04179ca1be2de5f77ce3d2
han-see <74469036+han-see@users.noreply.github.com>
2022-04-28 19:30:00 +0200

    Add moralis https url

--- src/config/RpcEndpoints.ts
+++ src/config/RpcEndpoints.ts
@@ -1,6 +1,9 @@
 // Moralis Endpoint Hansee
-export const MORALIS =
+export const MORALIS_WS =
   'wss://speedy-nodes-nyc.moralis.io/5a4d76c5b7eb6c76fe360efc/avalanche/mainnet/ws';
+
+export const MORALIS =
+  'https://speedy-nodes-nyc.moralis.io/5a4d76c5b7eb6c76fe360efc/avalanche/mainnet';
 
 // Default AVAX RPC endpoint
 export const AVAX_NODE = 'https://api.avax.network/ext/bc/C/rpc';
