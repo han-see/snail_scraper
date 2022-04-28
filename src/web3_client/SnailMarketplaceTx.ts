5d2db1cad229b406593d869b0b94da0d1ba066f7
han-see <74469036+han-see@users.noreply.github.com>
2022-04-28 19:29:50 +0200

    Add signing method and refactor class

--- src/web3_client/SnailMarketplaceTx.ts
+++ src/web3_client/SnailMarketplaceTx.ts
@@ -6,49 +6,55 @@ import { AVAX_NODE } from '../config/RpcEndpoints';
 export class SnailMarketplaceTx {
   private marketplaceContract = '0xeb77bd67Bd607e5b7d9b78db82fad0DE395B5DeF';
   private wallet: Wallet;
+  private walletAddress: string;
   private provider: JsonRpcProvider;
 
-  constructor() {
+  constructor(account: Account) {
     this.provider = new JsonRpcProvider(AVAX_NODE);
+    this.wallet = account.wallet;
+    this.walletAddress = account.walletAddress;
   }
 
-  async buySnailFromMarketplace(marketId: string, snailPrice: string) {
-    await Account.loadAccount()
-      .then((res) => {
-        this.wallet = res;
-      })
-      .catch((err) => console.error(err));
-
-    const walletAddress = await this.wallet.getAddress();
-    const walletNonce = await this.provider.getTransactionCount(walletAddress);
-    const maxPriorityFeePerGas = await this.provider
-      .getFeeData()
-      .then((data) => data.maxPriorityFeePerGas);
+  async buySnailFromMarketplace(
+    marketId: string,
+    snailPrice: string,
+  ): Promise<Transaction> {
+    const walletNonce = await this.provider.getTransactionCount(
+      this.walletAddress,
+    );
+    const feeData = await this.provider.getFeeData();
     const hexMarketId = marketId;
     const hexData = this.createInputData(hexMarketId);
+    let txResponse;
 
-    const tx: Transaction = {
-      from: walletAddress,
+    const unSignedTx: Transaction = {
+      from: this.walletAddress,
       to: this.marketplaceContract,
       nonce: walletNonce,
-      // taken from contract tx.
-      gasLimit: BigNumber.from('160000'),
+      // gasLimit taken from the recent contract call from snowtrace.
+      gasLimit: BigNumber.from(160000),
+      maxFeePerGas: feeData.maxFeePerGas,
+      maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
       data: hexData,
       value: ethers.utils.parseEther(snailPrice),
       chainId: 43114,
-      maxPriorityFeePerGas: maxPriorityFeePerGas,
+      type: 2,
     };
 
-    const signer = await this.provider.getSigner();
-    await signer
-      .sendTransaction(tx)
+    const signedTx = await this.wallet.signTransaction(unSignedTx);
+
+    await this.provider
+      .sendTransaction(signedTx)
       .then((transaction) => {
         console.log('Transaction successful');
         console.log(transaction);
+        txResponse = transaction;
       })
       .catch((err) => {
         console.log('Transaction is not successful', err);
       });
+
+    return txResponse;
   }
 
   // Create a valid input data for the transaction
@@ -61,38 +67,3 @@ export class SnailMarketplaceTx {
     return paddedHexBuyFunction + hexMarketId;
   }
 }
-
-/* // Generic interaction with the crabada game contract. The hex data is the start or
-        // end game data.
-        // Gas is estimated from recent blocks, capped by a max value. If gas is too high,
-        // the request will be skipped.
-        sendCrabadaRequest: async function (hexData) {
-          const account = web3.eth.accounts.privateKeyToAccount(deobfuscate(this.obfuscatedSecret));
-          const recentGasPrice = await web3.eth.getGasPrice();
-          const recentPriceOk = parseInt(recentGasPrice) < parseInt(this.maxGasPrice)
-          if (!recentPriceOk) {
-            sendWebhook("Gas prices too high, will try again later");
-            return;
-          } else {
-            console.log("Gas price of " + recentGasPrice + " is less than max gas price " + this.maxGasPrice);
-          }
-          const unsignedTx = {
-            from: account.address,
-            gas: this.gasLimit,
-            maxFeePerGas: recentGasPrice,
-            to: this.gameContract,
-            data: hexData,
-            type: "0x02", // Crabada uses EIP-1559 tx.
-          };
-
-          console.log("signing tx");
-          const signedTx = await web3.eth.accounts.signTransaction(unsignedTx, account.privateKey);
-
-          try {
-            console.log("sending tx");
-            const sentTx = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-            console.log(sentTx);
-            return sentTx;
-          } catch (ex) {
-            sendWebhook("failed to send tx: " + ex);
-          } */
