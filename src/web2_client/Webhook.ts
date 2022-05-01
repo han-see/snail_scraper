a43f636a4fcd2c80f955c0ee616b19fcc4070e84
han-see <74469036+han-see@users.noreply.github.com>
2022-05-01 16:24:07 +0200

    Remove unneeded data for the discord message

--- src/web2_client/Webhook.ts
+++ src/web2_client/Webhook.ts
@@ -1,31 +1,30 @@
-import { MessageEmbed, MessageEmbedFooter, WebhookClient } from 'discord.js';
+import { MessageEmbed, WebhookClient } from 'discord.js';
 import 'dotenv/config';
 export class Webhook {
-  embed: MessageEmbed;
-  footer: MessageEmbedFooter;
-  webhookClient = new WebhookClient({ url: process.env.DISCORD_WEBBOOK });
+  private embed: MessageEmbed;
+  private userWebhookClient = new WebhookClient({
+    url: process.env.USER_DISCORD_WEBHOOK,
+  });
 
   constructor(
     private title: string,
     private content: string,
     private snailLink: string,
     private thumbnail: string,
-    private nextSnailPrice?: number,
   ) {
     this.embed = new MessageEmbed()
       .setTitle(this.title)
-      .setColor('#3D9140')
       .setDescription(this.content)
       .setURL(this.snailLink)
       .setThumbnail(this.thumbnail)
       .setTimestamp();
-    this.embed.footer = {
-      text: `Next Snail's price in Category: ${this.nextSnailPrice} AVAX`,
-    };
   }
 
-  sendMessage() {
-    this.webhookClient
+  sendMessageToUser(nextSnailPrice: number) {
+    this.embed.footer = {
+      text: `Next Snail's price in Category: ${nextSnailPrice} AVAX`,
+    };
+    this.userWebhookClient
       .send({
         content: `<@${process.env.DISCORD_USER_ID}>\n${this.embed.title}`,
         embeds: [this.embed],
