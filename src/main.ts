bf0085483ad32930cab0c6a86d4dcbbe17e9917b
han-see <74469036+han-see@users.noreply.github.com>
2022-05-01 16:22:17 +0200

    Delete old function and replace it with the runBot()

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,12 @@
-import { Scraper } from './web2_client/Scraper';
-import userInput from '../userInput.json';
-import { Account } from './config/Account';
+import { Account } from './global/Account';
+import { EventBot } from './bots/EventBot';
 
-const scraperList: Scraper[] = [];
-
-async function botLoop() {
-  // eslint-disable-next-line no-constant-condition
-  await initiateBot();
-  const runBot = true;
-  while (runBot) {
-    console.log('Running the loop');
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    scraperList.forEach((res) => res.scrapeMarketplace());
-  }
+function runBot() {
+  const account = new Account();
+  account.loadAccount().then(() => {
+    const eventBot = new EventBot(account);
+    eventBot.startBot();
+  });
 }
 
-async function initiateBot() {
-  try {
-    let isUserInputEmpty = true;
-    while (isUserInputEmpty) {
-      if (userInput.length == 0) {
-        console.log('No User Input');
-        process.exit(1);
-      } else {
-        isUserInputEmpty = false;
-      }
-    }
-    await userInput.forEach((data) => {
-      scraperList.push(new Scraper(data));
-    });
-  } catch (err) {
-    console.error('Bot cannot be initialize', err);
-  }
-}
-
-botLoop();
-//Account.makeNewAccount();
-//Account.loadAccount();
+runBot();
